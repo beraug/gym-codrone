6b471f6c37f64741e270fc1994aeb1bca4794a41
beraug <37693050+beraug@users.noreply.github.com>
2018-09-08 22:48:43 +1000

    Update __init__.py

--- envs/__init__.py
+++ envs/__init__.py
@@ -1,12 +1,2 @@
-import logging
-from gym.envs.registration import register
-
-logger = logging.getLogger(__name__)
-
-register(
-    id='CoDrone-v0',
-    entry_point='gym-codrone.envs:CoDroneEnv',
-    timestep_limit=100,
-    
-)
+from gym-codrone.envs.codrone_env import CoDroneEnv
 

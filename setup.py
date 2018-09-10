56c9d46607154abab7ae97e2c471b2a38e242cbb
beraug <37693050+beraug@users.noreply.github.com>
2018-09-10 10:44:04 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
-setup(name='gym-codrone',
+setup(name='gym_codrone',
       version='0.0.1',
-      install_requires=['gym>=0.2.3',
-                        'hfo_py>=0.2']
-)
+      install_requires=['gym','CoDrone','tensorflow','time','numpy']  # And any other dependencies foo needs
+)  

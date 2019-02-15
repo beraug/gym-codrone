70d592468dd793acf492fb9974486c2f2a2ef6fb
beraug <37693050+beraug@users.noreply.github.com>
2019-02-15 13:04:42 +1100

    Update Ros_Policy.py

--- ros/Ros_Policy.py
+++ ros/Ros_Policy.py
@@ -5,7 +5,6 @@ import tensorflow.contrib.slim as slim
 import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
-
 #Policy Based Agent 1
 def discount_rewards(r):
     #take 1D float array of rewards and compute discounted reward
@@ -53,78 +52,3 @@ class agent():
         self.epsilon = 0.9  # exploration constant
         self.gamma = 0.9      # discount factor
         epsilon_discount = 0.01 # 8 eps to reach 0.1
-        
-#Training the Agent
-tf.reset_default_graph() #Clear the Tensorflow graph.
-
-myAgent = agent(lr=1e-2,s_size=3,a_size=6,h_size=8) #Load the agent.
-
-total_episodes = 10000 #Set total number of episodes to train agent on.
-max_ep = 500
-update_frequency = 10
-
-init = tf.global_variables_initializer()
-
-# Launch the tensorflow graph
-with tf.Session() as sess:
-    sess.run(init)
-    i = 1
-    total_reward = []
-    total_lenght = []
-        
-    gradBuffer = sess.run(tf.trainable_variables())
-    for ix,grad in enumerate(gradBuffer):
-        gradBuffer[ix] = grad * 0
-        
-    while i < total_episodes:
-        s = env.reset()
-        running_reward = 0
-        ep_history = []
-        for j in range(max_ep):
-            #Probabilistically pick an action given our network outputs.
-            a_dist = sess.run(myAgent.output,feed_dict={myAgent.state_in:[s]})
-            a = np.random.choice(a_dist[0],p=a_dist[0])
-            a = np.argmax(a_dist == a)
-
-            s1,r,d,_ = env.step(a) #Get our reward for taking an action given.
-            ep_history.append([s,a,r,s1])
-            s = s1
-            running_reward += r
-            print("Total Learning Reward ="+str(r))
-            print("Episode number ="+str(i))
-            print("Step number ="+str(j))
-            print("Done? "+str(d))
-            print("Running_reward "+str(running_reward))
-            print("Done? "+str(d))
-        
-      
-            if d == True:
-                #Update the network.
-                ep_history = np.array(ep_history)
-                ep_history[:,2] = discount_rewards(ep_history[:,2])
-                feed_dict={myAgent.reward_holder:ep_history[:,2],
-                        myAgent.action_holder:ep_history[:,1],myAgent.state_in:np.vstack(ep_history[:,0])}
-                grads = sess.run(myAgent.gradients, feed_dict=feed_dict)
-                for idx,grad in enumerate(grads):
-                    gradBuffer[idx] += grad
-                print("Discounted_rewards ="+str(discount_rewards))
-
-                if i < total_episodes:
-                    feed_dict= dictionary = dict(zip(myAgent.gradient_holders, gradBuffer))
-                    _ = sess.run(myAgent.update_batch, feed_dict=feed_dict)
-                    for ix,grad in enumerate(gradBuffer):
-                        gradBuffer[ix] = grad * 0
-                
-                total_reward.append(running_reward)
-                total_lenght.append(j)
-                print("Total reward sum = "+str(np.sum(running_reward)))
-                                
-                break
-
-        
-        #Update our running tally of scores.
-        if i % 10 == 0:
-            print("Mean Total Reward = "+str(np.mean(total_reward)))
-        i += 1
-        s = env.reset()
-        d = False
